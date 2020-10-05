a295e02534d8a2adc00141dd7730379b86bbbfd2
Misaki1225 <70563610+Misaki1225@users.noreply.github.com>
2020-10-05 11:44:36 +0900

    課題提出
    条件分岐・演算子

---
+++ 1-3/main.js
@@ -0,0 +1,26 @@
+//問１
+let a;
+console.log(a !== 10);
+
+let b;
+if(10<=b>=20 && b%2 === 0){
+    console.log(b);
+}
+
+//問2
+let x =10
+if(10<=x>=20){
+    console.log('成功です');
+}else{
+    console.log('失敗です');
+}
+
+//問3
+let y =0
+if(y%2 === 0){
+    console.log('偶数です');
+}
+if(y%2 !== 0){
+    console.log('奇数です');
+}
+
