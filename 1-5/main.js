b970b9b58d763adbecdaaebac3b55676c152b235
Misaki1225 <70563610+Misaki1225@users.noreply.github.com>
2020-10-13 23:01:26 +0900

    課題提出
    関数

---
+++ 1-5/main.js
@@ -0,0 +1,6 @@
+function createJuice(fruits){
+    console.log('みかんを受け取りました。ジュースにして返します');
+    return fruits + 'ジュース';
+}
+let orangejuice = createJuice('みかん');
+console.log(orangejuice + 'が届きました。');
